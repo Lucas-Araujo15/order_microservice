839e2814922afc1d643f2718435b69341000cead
Lucas Araujo <lucasaraujo4188@gmail.com>
2024-08-01 14:00:19 -0300

    add service and controller

---
+++ src/main/java/br/com/alurafood/orders/controller/OrderController.java
@@ -0,0 +1,56 @@
+package br.com.alurafood.orders.controller;
+
+import br.com.alurafood.orders.dto.OrderDTO;
+import br.com.alurafood.orders.dto.StatusDTO;
+import br.com.alurafood.orders.service.OrderService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    @Autowired
+    private OrderService service;
+
+    @GetMapping
+    public List<OrderDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable("id") Long id) {
+        OrderDTO dto = service.findById(id);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> create(@RequestBody @Valid OrderDTO dto, UriComponentsBuilder uriBuilder) {
+        OrderDTO orderPlaced = service.create(dto);
+
+        URI address = uriBuilder.path("/orders/{id}").buildAndExpand(orderPlaced.getId()).toUri();
+
+        return ResponseEntity.created(address).body(orderPlaced);
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<OrderDTO> updateStatus(@PathVariable("id") Long id, @RequestBody StatusDTO status) {
+        OrderDTO dto = service.updateStatus(id, status);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping("/{id}/paid")
+    public ResponseEntity<Void> approvePayment(@PathVariable @NotNull Long id) {
+        service.approveOrderPayment(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
--- src/main/java/br/com/alurafood/orders/service/OrderService.java
+++ src/main/java/br/com/alurafood/orders/service/OrderService.java
@@ -1,10 +1,19 @@
 package br.com.alurafood.orders.service;
 
+import br.com.alurafood.orders.dto.OrderDTO;
+import br.com.alurafood.orders.dto.StatusDTO;
+import br.com.alurafood.orders.model.Order;
+import br.com.alurafood.orders.model.Status;
 import br.com.alurafood.orders.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +23,50 @@ public class OrderService {
 
     @Autowired
     private final ModelMapper modelMapper;
+
+    public List<OrderDTO> findAll() {
+        return repository.findAll().stream()
+                .map(o -> modelMapper.map(o, OrderDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public OrderDTO findById(Long id) {
+        Order order = repository.findById(id).orElseThrow(EntityNotFoundException::new);
+
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public OrderDTO create(OrderDTO dto) {
+        Order order = modelMapper.map(dto, Order.class);
+
+        order.setDateTime(LocalDateTime.now());
+        order.setStatus(Status.COMPLETED);
+        order.getItems().forEach(item -> item.setOrder(order));
+        Order saved = repository.save(order);
+
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public OrderDTO updateStatus(Long id, StatusDTO dto) {
+        Order order = repository.findByIdWithItems(id);
+
+        if (order == null) {
+            throw new EntityNotFoundException();
+        }
+
+        order.setStatus(dto.getStatus());
+        repository.updateStatus(dto.getStatus(), order);
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public void approveOrderPayment(Long id) {
+        Order order = repository.findByIdWithItems(id);
+
+        if (order == null) {
+            throw new EntityNotFoundException();
+        }
+
+        order.setStatus(Status.PAID);
+        repository.updateStatus(Status.PAID, order);
+    }
 }
